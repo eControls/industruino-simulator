93d11313472ad75cfe5200f0a8d8a0b6f1e6a01a
Yohann Bacha <y.bacha@live.fr>
2018-01-23 19:34:15 +0100

    Initial commit

---
+++ properties.py
@@ -0,0 +1,39 @@
+words = [{
+    "address" : 40000,
+    "bit" : 0,
+    "type" : "BOOLEAN"
+}, {
+    "address" : 40000,
+    "bit" : 1,
+    "type" : "BOOLEAN"
+}, {
+    "address" : 40000,
+    "bit" : 2,
+    "type" : "BOOLEAN"
+}, {
+    "address" : 40011,
+    "bit" : 0,
+    "type" : "TEMPERATURE"
+}, {
+    "address" : 40012,
+    "bit" : 0,
+    "type" : "TEMPERATURE"
+}, {
+    "address" : 40015,
+    "bit" : 0,
+    "type" : "PERCENT"
+}, {
+    "address" : 40028,
+    "bit" : 0,
+    "type" : "VOLTAGE"
+}, {
+    "address" : 40038,
+    "bit" : 0,
+    "type" : "TIME_SECONDS"
+}, {
+    "address" : 40044,
+    "bit" : 0,
+
+}]
+
+app_id = 2
